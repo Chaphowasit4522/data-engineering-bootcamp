2fa83deff4663b0014c9ed0cebfcfdcdf16890e0
Chaphowasit Mahayossanan <chaphowasit.work@gmail.com>
2023-08-27 02:16:22 +0000

    Update PKan

--- 03-data-lake-with-google-cloud-storage/examples/uploading-to-gcs/main.py
+++ 03-data-lake-with-google-cloud-storage/examples/uploading-to-gcs/main.py
@@ -16,10 +16,10 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     # The ID of your GCS object
     # destination_blob_name = "storage-object-name"
 
-    keyfile = os.environ.get("KEYFILE_PATH")
+    keyfile = "DEB2-loadingFilesToGCS-573114da5e26.json"
     service_account_info = json.load(open(keyfile))
     credentials = service_account.Credentials.from_service_account_info(service_account_info)
-    project_id = "dataengineercafe"
+    project_id = "essential-graph-395008"
 
     storage_client = storage.Client(
         project=project_id,
